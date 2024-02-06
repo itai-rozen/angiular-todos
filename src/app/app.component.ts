05c83a666ba9a2ca754451334219660ccaf48a63
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-06 19:15:05 +0200

    first angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-todos';
+  todos = [
+    {title: 'Todo 1', id: 1, isComplete: false},
+    {title: 'Todo 2', id: 2, isComplete: false},
+    {title: 'Todo 3', id: 3, isComplete: false},
+    {title: 'Todo 4', id: 4, isComplete: false},
+  ]
+  createTodo = (e : FormDataEvent) => {
+    e.preventDefault();
+    console.log('e: ', e)
+  }
 }
