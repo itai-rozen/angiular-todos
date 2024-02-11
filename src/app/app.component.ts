126f61609af45b8ec17014b12621322a2c35f28d
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-12 01:41:02 +0200

    implemented delete & create todo events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgFor } from '@angular/common';
-
+import { TodoComponent } from './components/Todo/todo.component';
+import { FormComponent } from './components/Form/form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor,TodoComponent, FormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,8 +17,12 @@ export class AppComponent {
     {title: 'Todo 3', id: 3, isComplete: false},
     {title: 'Todo 4', id: 4, isComplete: false},
   ]
-  createTodo = (e : FormDataEvent) => {
-    e.preventDefault();
-    console.log('e: ', e)
+
+  deleteTodoEvent(id: number|undefined) {
+    if (id)
+      this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+  createTodoEvent(title: string) {
+    this.todos.push({id: this.todos.length+1, title, isComplete: false})
   }
 }
