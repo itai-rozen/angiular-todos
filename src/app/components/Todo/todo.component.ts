447bf37d7f5a7ac192bcbe34ac50875d57154b25
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-12 13:33:13 +0200

    conditional renderring if todo is to be editted or not; added edit form as child component

--- src/app/components/Todo/todo.component.ts
+++ src/app/components/Todo/todo.component.ts
@@ -1,19 +1,31 @@
 import { Component, Input, Output, EventEmitter } from "@angular/core";
-
-
+import { EditFormComponent } from "../EditForm/editform.component";
+import type { Todo } from "../../app.component";
+export type UpdateArgs = {
+  prop : keyof Todo,
+  id: number,
+  content: string|boolean
+}
 @Component({
   selector: 'todo',
   standalone: true,
+  imports: [EditFormComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss',
   
 })
 
 export class TodoComponent {
-  @Input() singleTodo! : {title?: string, id?: number, isComplete?: boolean};   
+  
+  @Input() singleTodo! : Todo;   
   @Output() deleteTodoEvent = new EventEmitter<number|undefined>()
+  @Output() updateTodoEvent = new EventEmitter<UpdateArgs>()
   deleteTodo(id: number|undefined) {
     if (id)
       this.deleteTodoEvent.emit(id);
+  }
+  updateTodo(prop: keyof Todo, id: number|undefined, content: boolean|string) {
+    if (id)
+      this.updateTodoEvent.emit({prop, id, content});
   }
 }
