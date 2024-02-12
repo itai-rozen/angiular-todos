f7ad25c9142c42136812d563b3fc038529a0ea42
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-12 13:23:09 +0200

    added todo interface & an update event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,15 @@ import { NgFor } from '@angular/common';
 import { TodoComponent } from './components/Todo/todo.component';
 import { FormComponent } from './components/Form/form.component';
 import { EditFormComponent } from './components/EditForm/editform.component';
+import type { UpdateArgs } from './components/Todo/todo.component';
+
+export interface Todo {
+  title: string,
+  id: number,
+  isComplete: boolean,
+  isEdit: boolean
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -12,18 +21,29 @@ import { EditFormComponent } from './components/EditForm/editform.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  todos = [
-    {title: 'Todo 1', id: 1, isComplete: false},
-    {title: 'Todo 2', id: 2, isComplete: false},
-    {title: 'Todo 3', id: 3, isComplete: false},
-    {title: 'Todo 4', id: 4, isComplete: false},
+  todos: Todo[] = [
+    {title: 'Todo 1', id: 1, isComplete: false, isEdit: false},
+    {title: 'Todo 2', id: 2, isComplete: false, isEdit: false},
+    {title: 'Todo 3', id: 3, isComplete: false, isEdit: false},
+    {title: 'Todo 4', id: 4, isComplete: false, isEdit: false},
   ]
 
-  deleteTodoEvent(id: number|undefined) {
+  deleteTodoEvent = (id: number|undefined) => {
     if (id)
       this.todos = this.todos.filter(todo => todo.id !== id);
   }
-  createTodoEvent(title: string) {
-    this.todos.push({id: this.todos.length+1, title, isComplete: false})
+  createTodoEvent = (title: string) => {
+    this.todos.push({id: this.todos.length+1, title, isComplete: false, isEdit: false})
+  }
+  updateTodoEvent = ({prop, id, content}: UpdateArgs) => {
+    if (!id)
+      return
+    this.todos = this.todos.map((todo: any): Todo => {
+       if (todo.id === id) {
+        todo[prop] = content;
+       } 
+       return todo
+    })
+    console.log('todos: ', this.todos)
   }
 }
