c9de263eddb1d097546f0a87d77f27338241e930
itai rozen <itai.rozen@mail.huji.ac.il>
2024-02-13 23:41:39 +0200

    component works with formGroup & formControl

--- src/app/components/EditForm/editform.component.ts
+++ src/app/components/EditForm/editform.component.ts
@@ -1,21 +1,24 @@
 import { Component, Output, EventEmitter, Input } from "@angular/core";
-import { FormBuilder } from "@angular/forms";
+import { FormControl, ReactiveFormsModule, FormGroup } from "@angular/forms";
 import { Todo } from "../../app.component";
 @Component({
   selector: 'edit-form',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './editform.component.html',
   styles: ['h1 { color: yellow }']
 })
 
 export class EditFormComponent {
-  @Output() UpdateEvent = new EventEmitter<Todo>()
-  @Input() todo!: Todo
-  updateTitle() {
-    this.UpdateEvent.emit(this.todo)
-  }
-  changeTitle(value: string) {
-    this.todo.title = value ;
+  editFormGroup = new FormGroup({
+    editFormTitle : new FormControl('')
+  })
+  @Output() UpdateEvent = new EventEmitter<Todo>();
+  @Input() todo!: Todo;
+  updateTitle = () => this.UpdateEvent.emit(this.todo);
+
+  changeTitle = () => {
+    this.todo.title = this.editFormGroup.value.editFormTitle as string;
     this.todo.isEdit = false;
     this.updateTitle()
   }
